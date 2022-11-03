d3b2d069b172fe0368c89117809c9319d0b07720
Alison Lee <alison2686@gmail.com>
2022-11-03 15:42:06 -0700

    add routes and navbar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Home from './Pages/Home'
 import Cart from './Pages/Cart'
 import SingleProduct from './Pages/SingleProduct'
 import Sales from './Pages/Sales'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/single-product' element={<SingleProduct />} />
-          <Route path='/sale' element={<Sales />} />
+          <Route path='/sales' element={<Sales />} />
         </Routes>
       </BrowserRouter>
     </div>
