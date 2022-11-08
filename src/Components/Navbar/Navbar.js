486b968b99da58ecc790c5241cdd131d1eb7b693
Alison Lee <alison2686@gmail.com>
2022-11-08 14:24:56 -0800

    update navbar sections

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Logo from '../../Assests/logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <h1>Navbar</h1>
+    <div>
+      <div>
+        <img src={Logo} alt='fable logo' />
+      </div>
+      <div>
+        <p>Collections</p>
+        <p>Customer</p>
+        <p>Sales</p>
+      </div>
+      <div>Items</div>
+    </div>
   )
 }
 
