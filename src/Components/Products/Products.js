a6aa2344c45f080842648924c890b01820a8b7ca
Alison Lee <alison2686@gmail.com>
2022-11-08 15:44:31 -0800

    update Product.css

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import './Products.css'
 import Foto from '../../Assests/Foto.png'
+
 
 const Products = () => {
   return (
-    <div>
-        <div>
+    <div className='productCon'>
+        <div className='productFirstCon'>
             <img src={Foto} alt='Foto'/>
         </div>
-        <div>
+        <div className='productSecCon'>
             <h3>Jacket KLS Beige</h3>
+            <h1>$105</h1>
         </div>
     </div>
   )
