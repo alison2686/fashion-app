5586c1cd105ed9e8e1954cb42e1f35bb586c5775
Alison Lee <alison2686@gmail.com>
2022-11-08 15:29:41 -0800

    add Product photo and h3

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Foto from '../../Assests/Foto.png'
 
 const Products = () => {
   return (
-    <h1>Products!</h1>
+    <div>
+        <div>
+            <img src={Foto} alt='Foto'/>
+        </div>
+        <div>
+            <h3>Jacket KLS Beige</h3>
+        </div>
+    </div>
   )
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,6 +11,8 @@ const Home = () => {
       </div>
       <div className='homeSecondCon'>
         <Products />
+        <Products />
+        <Products />
       </div>
     </div>
   )
