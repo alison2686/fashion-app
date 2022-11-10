875f8560df67a69987d530036f41911372b1e3eb
Alison Lee <alison2686@gmail.com>
2022-11-10 15:38:34 -0800

    add Sum

--- src/Components/OrderCard/OrderCard.js
+++ src/Components/OrderCard/OrderCard.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import OrderFoto from '../../Assests/Rectangle 65.png'
 
 const OrderCard = () => {
   return (
-    <div>OrderCard</div>
+    <div>
+      <div>
+        <img src={OrderFoto} alt='order' />
+      </div>
+      <div>
+        <div>
+          <h1>Jacket KLS</h1>
+          <p></p>
+          <p></p>
+        </div>
+        <div>
+          <h1> Size: M</h1>
+          <h1>Color: Black</h1>
+          <h1>Quantity: 1</h1>
+        </div>
+        <div>
+          <h1>Price: $105</h1>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Sum/Sum.js
+++ src/Components/Sum/Sum.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Sum = () => {
   return (
-    <div>Sum</div>
+    <div>
+      <div>
+        <div>
+          <h1>Summary:</h1>
+          <h1> $340</h1>
+        </div>
+        <div>
+          <h1>Delivery:</h1>
+          <h1>$0</h1>
+        </div>
+        <div>
+          <h1>Promocode:</h1>
+          <h1>$0</h1>
+        </div>
+      </div>
+
+      <div>
+        <h1>Total</h1>
+        <h1>$340</h1>
+      </div>
+
+      <div>
+        <input placeholder='Enter Promocode'/>
+        <button>Apply</button>
+      </div>
+    </div>
   )
 }
 
