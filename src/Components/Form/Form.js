4e5f82daf042490ddeda44d0ee265f9b3cff8572
Alison Lee <alison2686@gmail.com>
2022-11-10 15:26:29 -0800

    add order Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,7 +2,45 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div>
+      <form>
+        <div>
+          <h4>City</h4>
+          <input placeholder='Enter City'/>
+          <h4>Address</h4>
+          <input placeholder='Enter Address'/>
+        </div>
+
+        
+        <div>
+          <h4>Recipient's details</h4>
+          <label>First and Last Name</label>
+          <input placeholder='Enter Details'/>
+          <label>Phone</label>
+          <input placeholder='Enter Phone'/>
+          <label>Email</label>
+          <input placeholder='Enter Email'/>
+        </div>
+
+        <div>
+          <h4>Payment Method</h4>
+
+          <button>Payment card</button>
+          <button>Cash on Delivery</button>
+
+        </div>
+
+        <div>
+          <input type='checkbox'/>
+          <p>I agree to the terms of the offer and the loyalty policy</p>
+        </div>
+
+        <div>
+          <button>Place an order</button>
+        </div>
+
+      </form>
+    </div>
   )
 }
 
--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Form from '../Components/Form/Form'
 import OrderCard from '../Components/OrderCard/OrderCard'
 import Sum from '../Components/Sum/Sum'
+import '../Styles/Cart.css'
 
 const Cart = () => {
   return (
